13abe0c77b55ffd515b2aa9d7b0f04907218066d
chandra99-tech <chandra99iit@gmail.com>
2025-12-16 14:12:17 +0530

    Add files via upload

---
+++ Day_01/Assignment1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pattern Programm");
+
+            int num2 = Convert.ToInt32 (Console.ReadLine());
+
+
+            for (int i = 0; i < num2; i++)
+            {
+                for (int j = 0; j < num2-i-1; j++)
+                    Console.Write(" ");
+
+                for (int j = 0; j < 2*i+1; j++) 
+                {
+                    if (j==0 || j==2*i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+            for (int i = num2-2; i >=0; i--)
+            {
+                for (int j = 0; j < num2 - i - 1; j++)
+                    Console.Write(" ");
+
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    if (j == 0 || j == 2 * i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+
+        }
+    }
+}
