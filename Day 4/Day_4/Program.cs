fd6fa8d8e35ec9a2257dcdb193e82ad648ae49e9
chandra99-tech <chandra99iit@gmail.com>
2025-12-19 17:40:43 +0530

    Added DAY 4 .Net Code and Assignment

---
+++ Day 4/Day_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+
+namespace Day_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Employee obj = new Employee();
+            obj.Eid = 1;
+            obj.Name = "Ayush";
+            obj.Address = "Pune";
+
+            Console.WriteLine($"Eid ={obj.Eid}");
+            Console.WriteLine($"Eid ={obj.Name}");
+
+        }
+    }
+    public class Employee
+    {
+        private int _Eid;
+        private string _Name;
+        private string _Address;
+
+        public int Eid
+        {
+            set
+            {
+                _Eid = value;
+            }
+            get
+            {
+                return _Eid;
+            }
+        }
+        public string Name
+        {
+            set
+            {
+                if (value != null)
+                {
+                    _Name = "Mr/Mrs. " + value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Credential");
+                }
+            }
+            get 
+            { 
+                return _Name;
+
+            }
+        }
+
+        public string Address
+        {
+            set
+            {
+                _Address = value;
+                Console.WriteLine($"Address ={_Address}");
+            }
+        }
+
+    }
+}
---
+++ Day 4/Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Delegates
+{
+    public delegate int DelegateAdd(int x, int y);
+    public delegate int DelegateSub(int x, int y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cmath obj = new Cmath();
+            DelegateAdd add = new DelegateAdd(obj.Add);
+            int addresult = add(10, 12);
+            Console.WriteLine(addresult);
+
+            DelegateSub sub = new DelegateSub(obj.Subtract);
+            int subresult = sub(10, 12);
+            Console.WriteLine(subresult);
+            
+        }
+    }
+    public class Cmath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y) { return x - y; }
+        
+    }
+
+}
