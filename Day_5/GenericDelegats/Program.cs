f14c1011685c114821b56c4df824ea32f5543fa8
chandra99-tech <chandra99iit@gmail.com>
2025-12-22 19:47:52 +0530

    Add files via upload

---
+++ Day_5/GenericDelegats/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericDelegats
+{
+    public delegate T MyDelegats<T>();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass<string> sample = new SampleClass<string>();
+            MyDelegats<SampleClass<string>> del = new MyDelegats<SampleClass<string>>(sample.SampleMethod);
+        }
+    }
+
+    public class SampleClass<T>
+    {
+        public void SampleMethod()
+        {
+            Console.WriteLine("Sample Method");
+        }
+    }
+}
---
+++ Day_5/ObserverPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ObserverPattern
+{
+    public delegate void NotifiyEventHandler(string message);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Publisher publisher = new Publisher();
+           Subscriber subscriber = new Subscriber();
+
+            publisher.Notifiy += subscriber.MethodA;
+            publisher.Notifiy += subscriber.MethodB;
+
+            publisher.NotifyHandler("The Grand Sale Is Live On Amazon Great Indain Sale");
+
+            publisher.Notifiy -= subscriber.MethodB;
+
+            publisher.NotifyHandler("Sale Upto 50-80% discount and Great Deals");
+        }
+    }
+
+    public class Subscriber
+    {
+        public void MethodA(string message)
+        {
+            Console.WriteLine($"Message Receieved : {message} via SMS");
+        }
+        public void MethodB(string message)
+        {
+            Console.WriteLine($"Message Receieved : {message} via Email");
+        }
+    }
+
+    public class Publisher
+    {
+
+        public event NotifiyEventHandler Notifiy;
+        public void NotifyHandler(string message)
+        {
+            Notifiy?.Invoke(message);
+        }
+    }
+}
